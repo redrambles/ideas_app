f6487a197abcd6425cd4d5e595e66bd472b8d479
Ann Cascarano <a.cascarano@gmail.com>
2018-04-07 20:28:12 -0400

    add passport and bcrypt - begin auth process

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
 const mongoose = require('mongoose');
 
 
@@ -17,6 +19,41 @@ router.get('/login', (req, res) =>{
 // User Login Route
 router.get('/register', (req, res) =>{
   res.render('users/register');
+});
+
+// Register form POST
+router.post('/register', (req, res) => {
+  let errors = [];
+  if (req.body.password != req.body.password2){
+    errors.push({
+      text: "Passwords do not match."
+    });
+  }
+
+  if (req.body.password.length < 4) {
+    errors.push({
+      text: "Password must be at least 4 characters"
+    });
+  }
+
+  if(errors.length > 0){
+    res.render('users/register', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    });
+  } else {
+    const newUser = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    };
+    console.log(newUser);
+    res.send('passed');
+  }
+
 });
 
 module.exports = router;
